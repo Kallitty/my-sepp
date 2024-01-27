2f1ca7f503375508bd1c41049b2e52a41259ce3a
Kall <samueluche432@gmail.com>
2024-01-27 13:35:39 +0100

    modified:   src/Quiz.jsx modified:   src/index.scss

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -1,4 +1,15 @@
+import { useState } from 'react'
+
 const Quiz = ({ questions }) => {
-  return <div classNmae='quiz-container'></div>
+  const [currentQuestion, setCurrentQuestuion] = useState(0)
+  const { questions, choices, correctAnswer } = questions[currentQuestion]
+  return (
+    <div className='quiz-container'>
+      <>
+        <span className='active-question-no'> {currentQuestion + 1}</span>
+        <span className='total-question-no'> {currentQuestion + 1}</span>
+      </>
+    </div>
+  )
 }
 export default Quiz
