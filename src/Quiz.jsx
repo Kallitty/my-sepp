245786d7b47deae5c3bc0faa2fafeed3c34221ca
Kall <samueluche432@gmail.com>
2024-01-28 16:10:48 +0100

    modified:   src/Quiz.jsx

--- src/Quiz.jsx
+++ src/Quiz.jsx
@@ -44,28 +44,32 @@ const Quiz = ({ questions }) => {
 
   return (
     <div className='quiz-container'>
-      <>
-        <span className='active-question-no'> {currentQuestion + 1}</span>
-        <span className='total-question'> /{questions.length}</span>
-        <h2> {question}</h2>
-        <ul>
-          {' '}
-          {choices.map((answer, index) => (
-            <li
-              onClick={() => onAnswerClick(answer, index)}
-              key={answer}
-              className={answerIdx === index ? 'selected-answer' : null}
-            >
-              {answer}
-            </li>
-          ))}
-        </ul>
-        <div className='footer'>
-          <button onClick={onClickNext} disabled={answerIdx === null}>
-            {currentQuestion === questions.length - 1 ? 'Finish' : 'Next'}
-          </button>
-        </div>
-      </>
+      {!showResult ? (
+        <>
+          <span className='active-question-no'> {currentQuestion + 1}</span>
+          <span className='total-question'> /{questions.length}</span>
+          <h2> {question}</h2>
+          <ul>
+            {' '}
+            {choices.map((answer, index) => (
+              <li
+                onClick={() => onAnswerClick(answer, index)}
+                key={answer}
+                className={answerIdx === index ? 'selected-answer' : null}
+              >
+                {answer}
+              </li>
+            ))}
+          </ul>
+          <div className='footer'>
+            <button onClick={onClickNext} disabled={answerIdx === null}>
+              {currentQuestion === questions.length - 1 ? 'Finish' : 'Next'}
+            </button>
+          </div>
+        </>
+      ) : (
+        <div></div>
+      )}
     </div>
   )
 }
