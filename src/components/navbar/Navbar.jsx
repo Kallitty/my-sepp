b5844a341019c1878024dce72e561d7efa7dd5b2
Kall <samueluche432@gmail.com>
2024-02-11 18:24:54 +0100

    modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import './navbar.css'
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
 
 const Navbar = () => {
-  return <div></div>
+  return (
+    <div className='sepp__navbar'>
+      <div className='sepp__navbar-links'></div>
+    </div>
+  )
 }
 
 export default Navbar
