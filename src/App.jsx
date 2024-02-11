64c2e0f8b3453e67b33f95edb1ef4093d044edc7
Kall <samueluche432@gmail.com>
2024-02-11 07:13:22 +0100

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Article, Brand, CTA, Feature, Navbar } from './components'
 
+import { Blog, Features, Footer, Header, Possibility, Sepp } from './containers'
+
 const App = () => {
-  return <div></div>
+  return (
+    <div className='App'>
+      <div className='gradient__bg'>
+        <Navbar />
+        <Header />
+      </div>
+    </div>
+  )
 }
 
 export default App
