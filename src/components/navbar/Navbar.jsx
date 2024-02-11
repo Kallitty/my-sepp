0fd64a55074fe96c155b0ce037e31e989af11147
Kall <samueluche432@gmail.com>
2024-02-11 20:56:25 +0100

    new file:   src/assets/seppbg.jpg 	new file:   src/assets/seppworldbg.jpg 	modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './navbar.css'
 import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import logo from './assets/seppbg.jpg'
 
 const Navbar = () => {
   return (
