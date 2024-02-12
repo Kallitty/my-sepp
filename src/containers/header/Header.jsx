af1a5cd589a86712927efacc18aa93ac88be5fbb
Kall <samueluche432@gmail.com>
2024-02-12 23:14:06 +0100

    new file:   src/assets/visited.png modified:   src/containers/header/Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './header.css'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <div className='sepp__header section__padding' id='home'>
+      <div className='sepp__header-content'>
+        <h1 className='gradient__text'>
+          Best Guide to Succeeding in Professional and Scholarship Exams...
+        </h1>
+      </div>
+    </div>
+  )
 }
 
 export default Header
