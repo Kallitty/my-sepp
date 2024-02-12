b438f74e43263c70a10107e1fcfcc9f410b5af9c
Kall <samueluche432@gmail.com>
2024-02-12 23:22:18 +0100

    modified:   src/containers/header/Header.jsx

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,6 +8,10 @@ const Header = () => {
         <h1 className='gradient__text'>
           Best Guide to Succeeding in Professional and Scholarship Exams...
         </h1>
+        <div className='sepp__header-content__input'>
+          <input type='email' placeholder='Your Email Address' />
+          <button type='button'>Get Started</button>
+        </div>
       </div>
     </div>
   )
