80dd71686c7cee4c04bf72cf6163917d8b17ad94
Kall <samueluche432@gmail.com>
2024-02-12 14:38:51 +0100

    modified:   src/components/navbar/Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
-// import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'
+import { RiMenu3Line, RiCloseLin, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/cutoutworld.png'
+import { useState } from 'react'
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
   return (
     <div className='sepp__navbar'>
       <div className='sepp__navbar-links'>
@@ -21,6 +24,21 @@ const Navbar = () => {
       <div className='sepp__navbar-sign'>
         <p>Login</p>
         <button type='button'>Sign up</button>
+      </div>
+      <div className='sepp__navbar-menu'>
+        {toggleMenu ? (
+          <RiCloseLine
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color='#fff'
+            size={27}
+            onClick={() => setToggleMenu(true)}
+          />
+        )}
       </div>
     </div>
   )
