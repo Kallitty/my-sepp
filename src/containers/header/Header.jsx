c1823c1e8837deadf4c1fd9f279b0c1ac9322214
Kall <samueluche432@gmail.com>
2024-02-13 02:40:57 +0100

    modified:   src/containers/header/Header.jsx modified:   src/containers/header/header.css

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -8,14 +8,14 @@ const Header = () => {
     <div className='sepp__header section__padding' id='home'>
       <div className='sepp__header-content'>
         <h1 className='gradient__text'>
-          Best Guide to Succeeding in Professional and Scholarship Exams...
+          Best Guide to Succeed in Professional and Scholarship Exams...
         </h1>
         <p>
           Welcome to the ultimate destination for mastering professional and
           scholarship exams. Whether you're aiming for that dream job, pursuing
           higher education opportunities, or striving for academic excellence,
-          our comprehensive guide is designed to equip you with the knowledge,
-          strategies, and confidence needed to excel.
+          our comprehensive guide and practice questions are designed to equip
+          you with the knowledge, strategies, and confidence needed to excel.
         </p>
         <div className='sepp__header-content__input'>
           <input type='email' placeholder='Your Email Address' />
@@ -26,7 +26,7 @@ const Header = () => {
           <p> 32,448 people visited in the last 7 days</p>
         </div>
         <div className='sepp__header-image'>
-          {/* <img src={vrexam} alt='vrexam' /> */}
+          <img src={vrexam} alt='vrexam' />
         </div>
       </div>
     </div>
