247194c030591ae60e8acaae77ee6d96b0de6c91
Kall <samueluche432@gmail.com>
2024-02-14 20:42:01 +0100

    modified:   src/components/feature/Feature.jsx

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 import './feature.css'
 
 const Feature = () => {
-  return <div></div>
+  return (
+    <div className='sepp__features-container__feature'>
+      <div className='sepp__features-container__feature-title'>
+        <div />
+        <h1>{title}</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Feature
