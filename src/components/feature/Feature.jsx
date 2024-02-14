66e3a82428102d01b6941f93e7de1a2d8ce00100
Kall <samueluche432@gmail.com>
2024-02-14 22:45:07 +0100

    modified:   src/components/feature/Feature.jsx 	modified:   src/containers/whatissepp/Whatsepp.jsx

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './feature.css'
 
-const Feature = () => {
+const Feature = ({ title, text }) => {
   return (
     <div className='sepp__features-container__feature'>
       <div className='sepp__features-container__feature-title'>
         <div />
         <h1>{title}</h1>
       </div>
+      <div className='sepp__feature-container_feature-text'>{text}</div>
     </div>
   )
 }
--- src/containers/whatissepp/Whatsepp.jsx
+++ src/containers/whatissepp/Whatsepp.jsx
@@ -6,7 +6,12 @@ const Whatsepp = () => {
   return (
     <div className='sepp__whatsepp section__margin' id='wsepp'>
       <div className='sepp__whatsepp-feature'>
-        <Feature />
+        <Feature
+          title={'Who needs SEPP?'}
+          text={
+            "Everybody, Lol. But specifically, job seekers, professionals, and students preparing for exams benefit from the SEPP app's tailored study materials, practice tests, and exam preparation resources, enhancing their chances of success. "
+          }
+        />
       </div>
       <div className='sepp__whatsepp-heading'>
         <h1 className='gradient__text'>
@@ -15,6 +20,13 @@ const Whatsepp = () => {
         <p>Explore the resources...</p>
       </div>
       <div className='sepp__whatsepp-container'>
+        <Feature
+          title={'Who needs SEPP?'}
+          text={
+            "Everybody, Lol. But specifically, job seekers, professionals, and students preparing for exams benefit from the SEPP app's tailored study materials, practice tests, and exam preparation resources, enhancing their chances of success. "
+          }
+        />
+        <Feature />
         <Feature />
       </div>
     </div>
