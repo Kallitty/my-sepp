e28d9a828916e2401a5271c5f7e05e9d3ad17839
Kall <samueluche432@gmail.com>
2024-02-17 15:10:29 +0100

    new file:   public/sivideos/introvideo.mp4 	new file:   public/sivideos/loginvideo.mp4 	modified:   src/containers/features/Features.jsx 	modified:   src/containers/features/features.css 	modified:   src/containers/whatissepp/whatsepp.css

--- src/containers/features/Features.jsx
+++ src/containers/features/Features.jsx
@@ -16,8 +16,8 @@ const featueresData = [
     text: "When it comes to exam preparation, quality matters. That's why we've curated a comprehensive collection of resources to help you succeed. From detailed examination guides to practice questions and study tips, you'll find everything you need to prepare effectively and confidently.",
   },
   {
-    title: 'Community Engagement',
-    text: "Preparing for a professional exam is a journey best shared with others. That's why we've built a vibrant community where you can connect with peers, ask questions, and share insights. Join the conversation on our forums, discussion boards, or social media channels and tap into the collective wisdom of your fellow professionals, job seekers, and students.",
+    title: 'Community',
+    text: "Preparing for a professional exam is a journey best shared with others. That's why we've built a vibrant community where you can connect with peers, ask questions, and share insights. Join the conversation on our forums, discussion boards, or social media channels and tap into the collective wisdom.",
   },
 ]
 
@@ -26,8 +26,8 @@ const Features = () => {
     <div className='sepp__features section__padding' id='features'>
       <div className='sepp__features-heading'>
         <h1 className='gradient__text'>
-          Your Best Career Choice Is Now,We Are Rooting For You To Achieve That.
-          You Need To Step In & Make It Happen.
+          Your Best Career Choice Is Now, We Are Rooting For You To Succeed. You
+          Need To Step In & Make It Happen.
         </h1>
         <p> Request Early Access to Make it Happen</p>
       </div>
