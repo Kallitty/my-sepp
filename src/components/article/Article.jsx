79e9f2f0f1ec794ec4f56c9f1a1a0ccec101fe01
Kall <samueluche432@gmail.com>
2024-02-18 09:24:07 +0100

    modified:   src/components/article/Article.jsx modified:   src/containers/blog/Blog.jsx

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 import './article.css'
 
 const Article = () => {
-  return <div></div>
+  return (
+    <div className='sepp__blog-container_article'>
+      <div className='sepp__blog-contaner_article-image'>
+        <img src={imageUrl} alt=' blogImage' />
+      </div>
+    </div>
+  )
 }
 
 export default Article
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './blog.css'
+import Article from '../../components/article/Article.jsx'
+
 const Blog = () => {
   return (
     <div className='sepp__blog section__padding' id='blog'>
       <div className='sepp__blog-heading'>
         <h1 className='gradient__text'>Explore Our Blog</h1>
+        <div className='sepp__blog-container'>
+          <div className='sepp__container_groupA'>
+            <Article />
+          </div>
+          <div className='sepp__container_groupB'>
+            <Article />
+            <Article />
+            <Article />
+            <Article />
+          </div>
+        </div>
       </div>
     </div>
   )
