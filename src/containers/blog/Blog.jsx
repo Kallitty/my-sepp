9ed17083697f85f05712305e10a602ffe69e30e2
Kall <samueluche432@gmail.com>
2024-02-18 08:36:47 +0100

    modified:   src/containers/blog/Blog.jsx

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import './blog.css'
 const Blog = () => {
-  return <div></div>
+  return (
+    <div className='sepp__blog section__padding' id='blog'>
+      <div className='sepp__blog-heading'>
+        <h1 className='gradient__text'>Explore Our Blog</h1>
+      </div>
+    </div>
+  )
 }
 
 export default Blog
