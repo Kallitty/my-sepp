9940744d389c6a36a7796b16e66580e4c7cd4fa4
Kall <samueluche432@gmail.com>
2024-02-18 19:54:16 +0100

    modified:   src/components/article/Article.jsx

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import './article.css'
 
-const Article = ({ imgUrl }) => {
+const Article = ({ imgUrl, date, title }) => {
   return (
     <div className='sepp__blog-container_article'>
       <div className='sepp__blog-container_article-image'>
         <img src={imgUrl} alt=' blogImage' />
       </div>
+      <div className='sepp__blog-container_article-content'></div>
+      <p>{date}</p>
+      <h3>{title}</h3>
+      <p>Read Full Article</p>
     </div>
   )
 }
