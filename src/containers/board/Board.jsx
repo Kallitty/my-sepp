c31398613d5cafab8c4261db9b5ba40b9d6311ed
Kall <samueluche432@gmail.com>
2024-03-04 15:09:27 +0100

    Changes to be committed: 	modified:   src/components/index.js 	modified:   src/containers/board/Board.jsx

--- src/containers/board/Board.jsx
+++ src/containers/board/Board.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Sidebar from '../../components/sidebar'
+import { Sidebar } from '../../components/'
 const Board = () => {
   return (
     <div className='sepp__board'>
