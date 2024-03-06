ad98552fa3f27e551d909d40e22b158e9315b24c
Kall <samueluche432@gmail.com>
2024-03-06 21:54:17 +0100

    modified:   src/components/content/Content.jsx 	modified:   src/components/content/content.scss 	modified:   src/components/contentheader/ContentHeader.jsx 	deleted:    src/components/contentheader/content.scss 	new file:   src/components/contentheader/contentheader.scss

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import './content.scss'
+import ContentHeader from '../contentheader/ContentHeader'
 
 const Content = () => {
-  return <div>Content</div>
+  return (
+    <div className='sepp__content'>
+      <ContentHeader />
+    </div>
+  )
 }
 
 export default Content
