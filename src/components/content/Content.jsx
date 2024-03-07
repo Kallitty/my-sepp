2da88bb3deef5e150bbccdab04a88eb78f4d1b41
Kall <samueluche432@gmail.com>
2024-03-07 05:40:56 +0100

    new file:   src/components/card/Card.jsx 	new file:   src/components/card/card.scss 	modified:   src/components/content/Content.jsx 	modified:   src/components/contentheader/contentheader.scss

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './content.scss'
 import ContentHeader from '../contentheader/ContentHeader'
+import Card from '../card/Card'
 
 const Content = () => {
   return (
     <div className='sepp__content'>
       <ContentHeader />
+      <Card />
     </div>
   )
 }
