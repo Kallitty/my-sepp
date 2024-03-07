a5e6634c56ad6bb5f7631842c4b50a34978421d4
Kall <samueluche432@gmail.com>
2024-03-07 17:27:18 +0100

    modified:   src/components/card/Card.jsx 	modified:   src/components/card/card.scss

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,24 @@
 import React from 'react'
+import { FaBook, FaVideo, FaFileAlt } from 'react-icons/fa'
+import './card.scss'
+
+const course = [
+  { id: 1, title: 'Study Ebook', icon: <FaBook /> },
+  { id: 2, title: 'Study Videos', icon: <FaVideo /> },
+  { id: 3, title: 'Others', icon: <FaFileAlt /> },
+]
 
 const Card = () => {
-  return <div>Card</div>
+  return (
+    <div className='sepp__card-container'>
+      {course.map((item) => (
+        <div className='sepp__card'>
+          <div className='sepp__card--cover'>{item.icon}</div>
+          <div className='sepp__card--title'>{item.title}</div>
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Card
