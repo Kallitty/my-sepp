8d6441224e238b5935e6151d92eb5e2edba9d959
Kall <samueluche432@gmail.com>
2024-03-09 14:44:14 +0100

    Edited   src/components/tutorlist/TutorList.jsx

--- src/components/tutorlist/TutorList.jsx
+++ src/components/tutorlist/TutorList.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import './tutorlist.scss'
 import { Profil } from './importtl.js'
 
-const teachers = [
+const tutors = [
   { image: Profil, name: 'Dr U. Psalms', duration: '6 hours', cost: '99.90' },
 
   { image: 'Image45', name: 'Prof M. Edu', duration: '4 hours', cost: '99.80' },
@@ -18,6 +18,19 @@ const TutorList = () => {
           <option value='French'>French</option>
           <option value='Spanish'>Spanish</option>
         </select>
+      </div>
+      <div className='sepp__tutorlist-container'>
+        {tutors.map((tutor) => (
+          <div className='sepp__tutorlist--list'>
+            <div className='sepp__tutorlist--tutor-detail'>
+              <img src={tutor.image} alt={tutor.name} />
+              <h2>{tutor.name}</h2>
+            </div>
+            <span>{tutor.duration}</span>
+            <span>{tutor.cost}</span>
+            <span className='sepp__tutorlist--tutor-todo'>:</span>
+          </div>
+        ))}
       </div>
     </div>
   )
