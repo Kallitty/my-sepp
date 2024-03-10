2d98fc925052b5c1cfbe367be9fb94fa9e9cf0ca
Kall <samueluche432@gmail.com>
2024-03-10 09:56:51 +0100

    modified:   src/components/profile/Profile.jsx

--- src/components/profile/Profile.jsx
+++ src/components/profile/Profile.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './profile.scss'
 import ProfileHeader from '../profileheader/ProfileHeader'
+import { Profil } from './importp.js'
 
 const Profile = () => {
   return (
     <div className='sepp__profile'>
       <ProfileHeader />
+      <div className='sepp__profile-user--profile'>
+        <div className='sepp__profile-user--detail'>
+          <img src={Profil} alt='' />
+        </div>
+      </div>
     </div>
   )
 }
